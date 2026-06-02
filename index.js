d1ca8726f9df9ee090bdcd8a8d6ca2996d52102b
Karthik <thalakantikarthikkumarreddy@gmail.com>
2026-06-02 10:34:37 +0530

    c2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World i am karthik')
 })
 
 app.listen(3000, () => {
